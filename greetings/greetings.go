7d55d1afb74cb146d63b03f91865359837605f43
Rhys <rhyssmith37@gmail.com>
2022-07-17 21:24:09 +0100

    feat: adding functions for generating a random number
    * Adding private functions for generating a random greeting message
    * Used the init function to set the seed of generating the random number

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns greeting for the named person.
@@ -14,6 +16,26 @@ func Hello(name string) (string, error) {
 
 	// If a name was received, return a value that embeds the name
 	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randonFormat returns one of a set of greeting messges. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Weel met!",
+	}
+
+	// Returns a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
